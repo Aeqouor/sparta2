9fba420fddb010f06f7a7af6d73f64204784e4b5
minhuieun <vitaars@naver.com>
2024-11-15 16:03:30 +0900

    Initial commit

---
+++ src/main/java/org/example/sparta2/Sparta2Application.java
@@ -0,0 +1,13 @@
+package org.example.sparta2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sparta2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sparta2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sparta2/Sparta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sparta2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sparta2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
